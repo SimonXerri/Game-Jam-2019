887e462f4d257062c6bc422e81d1496baf4b69bb
RussellSB <r.sammutbonnici@gmail.com>
2019-11-29 22:53:13 +0100

    Basic movements

---
+++ QualityEducation/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 9;                         // Speed of player
+    public float acceleration = 75;                 // Acceleration when starting to walk
+    public float deceleration = 70;                 // Deceleration when slowing down
+    private Vector3 velocity;                       // To store x and y velocity at the current update
+
+    void FixedUpdate()
+    {
+        float moveHor = Input.GetAxisRaw("Horizontal");
+        float moveVer = Input.GetAxisRaw("Jump");
+
+        // Horzontal Movement
+        if (moveHor != 0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * moveHor, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, deceleration * Time.deltaTime);
+        }
+
+        // Vertical Movement
+        if (moveVer != 0)
+        {
+            velocity.y = Mathf.MoveTowards(velocity.y, speed * moveVer, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.y = Mathf.MoveTowards(velocity.y, 0, deceleration * Time.deltaTime);
+        }
+
+        transform.Translate(velocity * Time.deltaTime);
+    }
+}
