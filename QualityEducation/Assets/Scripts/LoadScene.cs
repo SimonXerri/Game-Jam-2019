968dc104790c19a415b0bbf76775cbfe2944029d
GabrielMicallef <58345316+GabrielMicallef@users.noreply.github.com>
2019-11-30 14:49:54 +0100

    Game UI

---
+++ QualityEducation/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+    
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+    public void Quit(){
+        Debug.Log("The game will be closed");
+        Application.Quit();
+    }
+}
---
+++ QualityEducation/Assets/Scripts/QuitScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("The game will be closed.");
+        Application.Quit();
+    }
+}
