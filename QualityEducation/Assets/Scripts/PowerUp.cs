e7bbbfff6498a0adca3b99a28f4ffd25ec498273
SimonXerri <simon.xerri@gmail.com>
2019-11-30 14:25:22 +0100

    push with test level

---
+++ QualityEducation/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public PlayerController pc;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+
+            if (pc.speed < 18)
+            {
+                pc.speed *= 2;
+            }
+
+            
+        }
+    }
+
+}
